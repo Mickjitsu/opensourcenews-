92ae70c7909d5b84bcb36d9d414a644987db3382
Michael Mc Court <mmccourt928@outlook.com>
2024-12-12 19:37:15 +0000

    debug updated

--- opensourcenews/opensourcenews/settings.py
+++ opensourcenews/opensourcenews/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-mickjitsu-opensourcenew-23zhy7ynqgk.ws.codeinstitute-ide.net',
 '.herokuapp.com',]
